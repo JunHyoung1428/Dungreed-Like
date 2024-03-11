8a2ba9f47f37cf5d5a76ce1599954775e5d13de2
Park JunHyoung <matte1428@naver.com>
2024-03-11 20:55:08 +0900

    update : Player 공격 개선
    ToDo : 공격시 이펙트

--- Assets/_Scripts/Player/PlayerAttack.cs
+++ Assets/_Scripts/Player/PlayerAttack.cs
@@ -1,39 +1,43 @@
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [SerializeField] Transform body;
     [SerializeField] Transform handPos;
     [SerializeField] GameObject weapon;
-    [SerializeField] SpriteRenderer body;
 
-    [SerializeField] float rotationAngle = 120.0f;
+    [SerializeField] float rotationAngle;
+    [SerializeField] float attackSpeed;
     [SerializeField] float attackDuration;
     [SerializeField] bool attackForm;
     [SerializeField] bool isAttack;
 
+
+    private Vector3 dir;
     /******************************************************
      *                      Unity Events
      ******************************************************/
 
-
-    //마우스 따라서 손 회전
-    // ** 회전이 아래에서만 이뤄짐 
     void Update()
     {
+        dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - handPos.position;
         if (!isAttack)
             Aim();
     }
 
     void Aim()
     {
-        Vector3 dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - handPos.position;
         if (dir != Vector3.zero)
         {
+            
             dir.z = 0f;
             //handPos.transform.LookAt2DLerp(dir); // x스케일 -1 일때 회전이 반대 방향으로 이뤄짐
-            handPos.up = dir.normalized;
+           handPos.localScale = new Vector3(body.localScale.x, handPos.localScale.y, handPos.localScale.z);
+           handPos.right = dir.normalized;
         }
     }
     /******************************************************
@@ -41,8 +45,6 @@ public class PlayerAttack : MonoBehaviour
      ******************************************************/
 
     //일단은 장병기 구현
-    // 어쩌다보니 손 움직이는 스크립트가 되어버림...
-    //공격시 일정각도 회전 + 카메라 쉐이킹 + 폼에 따라 레이어 변경해서 캐릭터 몸 앞/뒤로 전환
     void OnAttack(InputValue inputValue)
     {
         if (inputValue.isPressed && !isAttack)
@@ -51,25 +53,23 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
+    //TODO:공격시 이펙트 오브젝트 풀링으로 생성
     IEnumerator AttackRoutine()
     {
         isAttack = true;
-        float currentZRotation = handPos.rotation.eulerAngles.z;
-        float targetZRotation;
-        if (attackForm)
+        if(attackForm)
         {
-            targetZRotation = currentZRotation + rotationAngle;
+            Debug.Log("Attack1");
+            handPos.localScale = new Vector3(handPos.localScale.x, -1, handPos.localScale.z);
             attackForm = false;
         }
         else
         {
-            targetZRotation = currentZRotation - rotationAngle;
+            Debug.Log("Attack2");
+            handPos.localScale = new Vector3(handPos.localScale.x, 1, handPos.localScale.z);
             attackForm = true;
         }
-        handPos.rotation = Quaternion.Euler(handPos.rotation.eulerAngles.x, handPos.rotation.eulerAngles.y, targetZRotation);
-        //handPos.rotation = Quaternion.Slerp(handPos.rotation, targetZRotation, Time.deltaTime);
-
         yield return new WaitForSeconds(attackDuration);
-        isAttack = false;
+        isAttack =false;
     }
 }
