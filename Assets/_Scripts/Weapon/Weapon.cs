e2c61d97b4206a6cde49ba0cd074751f6f2f77b1
Park JunHyoung <matte1428@naver.com>
2024-03-12 20:54:44 +0900

    feat :  Weapon

--- Assets/_Scripts/Weapon/Weapon.cs
+++ Assets/_Scripts/Weapon/Weapon.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,22 +7,31 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField] protected Transform handPos;
     [SerializeField] protected PooledObject effect;
+    [SerializeField] protected CinemachineImpulseSource impulse;
+
 
     [SerializeField] protected float attackSpeed;
     [SerializeField] protected float attackDuration;
 
+    [SerializeField] protected int damage;
+
     [SerializeField] protected bool isAttack;
 
 
-    private void Start()
+    public virtual void Start()
     {
         Manager.Pool.CreatePool(effect, 5, 5);
     }
-
+    
 
     public virtual void Attack(Vector3 dir)
     {
         if (isAttack) return;
+    }
+
+    protected void Shake(float roughness)
+    {
+        impulse.GenerateImpulseWithForce(roughness/20);
     }
 
 }
